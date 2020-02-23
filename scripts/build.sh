1dd29bbe2e30dc07b16c9d6a3671556f21d786e8
stdmatt <stdmatt@pixelwizards.io>
2020-02-23 21:00:10 +0300

    Change the build script with the paths of GBDK for OSX.

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,21 @@
 #!/usr/bin/env bash
+##~---------------------------------------------------------------------------##
+##                        _      _                 _   _                      ##
+##                    ___| |_ __| |_ __ ___   __ _| |_| |_                    ##
+##                   / __| __/ _` | '_ ` _ \ / _` | __| __|                   ##
+##                   \__ \ || (_| | | | | | | (_| | |_| |_                    ##
+##                   |___/\__\__,_|_| |_| |_|\__,_|\__|\__|                   ##
+##                                                                            ##
+##  File      : build.sh                                                      ##
+##  Project   : flappy_gb                                                     ##
+##  Date      : Sep 22, 2019                                                  ##
+##  License   : GPLv3                                                         ##
+##  Author    : stdmatt <stdmatt@pixelwizards.io>                             ##
+##  Copyright : stdmatt 2019, 2020                                            ##
+##                                                                            ##
+##  Description :                                                             ##
+##                                                                            ##
+##---------------------------------------------------------------------------~##
 
 ##----------------------------------------------------------------------------##
 ## Imports                                                                    ##
@@ -21,9 +38,16 @@ SRC_DIR="$PROJECT_DIR/src";
 ##----------------------------------------------------------------------------##
 cd "$PROJECT_DIR";
 
+## @todo(stdmatt): Make the project compilers on GNU/Linux as well..
+if [ $(pw_os_get_simple_name) == $(PW_OS_OSX) ]; then
+    GBDKDIR="${PROJECT_DIR}/third_party/osx/gbdk";
+    LCC="${GBDKDIR}/bin/lcc";
+fi;
+
 rm    -rf "$BUILD_DIR";
 mkdir -p  "$BUILD_DIR";
-lcc -o $BUILD_DIR/flappy.gb  $SRC_DIR/*.c  $INCLUDE_DIR/res/flappy.c
+
+$LCC -o $BUILD_DIR/flappy.gb  $SRC_DIR/*.c  $INCLUDE_DIR/res/flappy.c
 
 if [ "$1" == "--run" ]; then
     mgba -3 ./build/flappy.gb
