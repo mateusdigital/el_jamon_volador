702799658c68ff718835d11040e42f2f18534d24
stdmatt <stdmatt@pixelwizards.io>
2020-05-27 20:41:02 +0300

    Remove old code.

--- html/app.js
+++ html/app.js
@@ -10,30 +10,17 @@ const ROM_FILENAME="./rom/game.gb"
 
 var gameboy = window.gameboy = new (window.Gameboy)();
 
+//
 // Render
 var canvas = document.getElementById('frame');
 var ctx    = canvas.getContext('2d');
+
 canvas.style.width  = "100%";
 canvas.style.height = "100%";
 
-var topMap = new Image();
-topMap.src = "./img/frame.png";
-
 gameboy.gpu.on('frame', function (offcanvas) {
     console.log(offcanvas.width, offcanvas.height);
-    // ctx.fillStyle = "#161425";
-    // ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-    //ctx.drawImage(topMap, 0, 0)
-    // ctx.save();
-    // ctx.translate(topMap.width * 0.5, topMap.height * 0.5);
-    //ctx.scale(3.0, 3.0);
-    ctx.drawImage(
-        offcanvas,
-        -offcanvas.width  * 0.0,
-        -offcanvas.height * 0.0
-    );
-    //ctx.restore();
-
+    ctx.drawImage(offcanvas, 0, 0);
 });
 
 //
