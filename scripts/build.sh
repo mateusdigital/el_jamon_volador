4eb8616c3e41f016232b6d92a6797b4067261cd9
mateus-earth <mateus@cosmicpig.digital>
2023-04-11 13:53:11 -0300

    Simplify script and make it build on mac

--- scripts/build.sh
+++ scripts/build.sh
@@ -20,39 +20,37 @@
 ##----------------------------------------------------------------------------##
 ## Imports                                                                    ##
 ##----------------------------------------------------------------------------##
-source /usr/local/src/stdmatt/shellscript_utils/main.sh
-
 
 ##----------------------------------------------------------------------------##
 ## Variables                                                                  ##
 ##----------------------------------------------------------------------------##
-SCRIPT_DIR="$(pw_get_script_dir)";
-ROOT_DIR=$(pw_abspath "${SCRIPT_DIR}/..");
-BUILD_DIR="${ROOT_DIR}/build";
-DIST_DIR="${ROOT_DIR}/dist";
-HTML_DIR="${ROOT_DIR}/html";
+readonly SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)";
+readonly ROOT_DIR="$(dirname "$SCRIPT_DIR")";
 
-SOURCE_DIR="${ROOT_DIR}/game/src";
-INCLUDE_DIR="${ROOT_DIR}/game/include";
-LCC="";
+readonly BUILD_DIR="${ROOT_DIR}/build";
+readonly DIST_DIR="${ROOT_DIR}/dist";
+readonly HTML_DIR="${ROOT_DIR}/html";
 
-PROJECT_PACKAGE_NAME="el_ramon_volador";
+readonly SOURCE_DIR="${ROOT_DIR}/game/src";
+readonly INCLUDE_DIR="${ROOT_DIR}/game/include";
+
 
 ##----------------------------------------------------------------------------##
 ## Script                                                                     ##
 ##----------------------------------------------------------------------------##
 ## @todo(stdmatt): Make the project compilers on GNU/Linux as well..
-if [ "$(pw_os_get_simple_name)" == "$(PW_OS_OSX)" ]; then
+if [ -n "$(uname -s | grep "Darwin")" ]; then
     #notice(stdamtt):  GBDKDIR NEEDS TO END WITH A SLASH /
     export GBDKDIR="${ROOT_DIR}/game/third_party/osx/gbdk/";
-    test -d "$GBDKDIR" ||            \
-        pw_log_fatal                 \
-            "GBDKDIR doesn't exists" \
-            "GBDKDIR: ($GBDKDIR)";
+    if [ ! -d "$GBDKDIR" ]; then
+        echo "GBDKDIR doesn't exists GBDKDIR: ($GBDKDIR)";
+        exit 1;
+    fi;
 
-    LCC="${GBDKDIR}/bin/lcc";
+    export LCC="${GBDKDIR}/bin/lcc";
 else
-    pw_log_fatal "Can't build in non OSX hosts yet...";
+    echo "Can't build in non OSX hosts yet...";
+    exit 1;
 fi;
 
 ##
@@ -61,39 +59,34 @@ echo "Cleaning build directory";
 rm    -rf "$BUILD_DIR";
 mkdir -p  "$BUILD_DIR";
 
-FINAL_VERSION="$(bump-the-version \
-    "${INCLUDE_DIR}/version.h"    \
-    "#define GAME_VERSION"        \
-    show                          \
-)";
-FULL_PACKAGE_NAME="${PROJECT_PACKAGE_NAME}_${FINAL_VERSION}.gb";
+FULL_PACKAGE_NAME="${PROJECT_PACKAGE_NAME}_${PROJECT_VERSION}.gb";
 
-echo "Building $FULL_PACKAGE_NAME";
+echo "Building $PROJECT_NAME - $FINAL_VERSION";
 ## Compile the game.
 $LCC -o                               \
     ${BUILD_DIR}/${FULL_PACKAGE_NAME} \
     ${SOURCE_DIR}/*.c                 \
     ${INCLUDE_DIR}/res/flappy.c
 
-##
-## HTML
-if [ -n  "$(pw_getopt_exists "--html" "$@")" ]; then
-    echo "Building HTML...";
-    cp -v ${BUILD_DIR}/${FULL_PACKAGE_NAME} ${HTML_DIR}/rom/game.gb
-fi;
-
-##
-## Dist
-test -z "$(pw_getopt_exists "--dist" "$@")" && exit;
-echo "Creating the distribution artifact!!!";
-
-echo "Cleaning dist directory";
-rm    -rf "$DIST_DIR";
-mkdir -p  "$DIST_DIR";
-
-echo "Creating zip file";
-ZIP_FILENAME="${PROJECT_PACKAGE_NAME}_v${FINAL_VERSION}.zip";
-pw_pushd "${BUILD_DIR}";
-    zip -r "${ZIP_FILENAME}" ".";
-    mv "${ZIP_FILENAME}" "${DIST_DIR}";
-pw_popd
+# ##
+# ## HTML
+# if [ -n  "$(pw_getopt_exists "--html" "$@")" ]; then
+#     echo "Building HTML...";
+#     cp -v ${BUILD_DIR}/${FULL_PACKAGE_NAME} ${HTML_DIR}/rom/game.gb
+# fi;
+#
+# ##
+# ## Dist
+# test -z "$(pw_getopt_exists "--dist" "$@")" && exit;
+# echo "Creating the distribution artifact!!!";
+#
+# echo "Cleaning dist directory";
+# rm    -rf "$DIST_DIR";
+# mkdir -p  "$DIST_DIR";
+#
+# echo "Creating zip file";
+# ZIP_FILENAME="${PROJECT_PACKAGE_NAME}_v${FINAL_VERSION}.zip";
+# pw_pushd "${BUILD_DIR}";
+#     zip -r "${ZIP_FILENAME}" ".";
+#     mv "${ZIP_FILENAME}" "${DIST_DIR}";
+# pw_popd
