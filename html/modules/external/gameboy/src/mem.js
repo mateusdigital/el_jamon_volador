0764b18ec12e7820107f57684540ca1e70f3942b
mateusdigital <mateus@cosmicpig.digital>
2023-05-29 13:23:48 -0300

    Add the gameboy emulador.

--- html/modules/external/gameboy/src/mem.js
+++ html/modules/external/gameboy/src/mem.js
@@ -598,8 +598,6 @@ const Mem = function (nes, cpu) {
             this.romName += String.fromCharCode (rom [i]);
         }
 
-        document.title = 'Pollen Boy: ' + this.romName;
-
         // ---- CGB MODE ---- //
         if (rom [0x143] === 0xc0)
             throw 'rom is gb color only :(';
