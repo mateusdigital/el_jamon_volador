9f8b182a13815e90a7f42d7b2cb32cea1e8395bb
mateusdigital <mateus@cosmicpig.digital>
2023-06-01 13:48:11 -0300

    Add the build-static and deploy scripts

--- scripts/build-static.sh
+++ scripts/build-static.sh
@@ -10,26 +10,21 @@
 ##                 +                         +                                ##
 ##                      O      *        '       .                             ##
 ##                                                                            ##
-##  File      : deploy-to-github-pages.sh                                     ##
+##  File      : build-static.sh                                               ##
 ##  Project   : flappy_gb                                                     ##
-##  Date      : May 05, 2023                                                  ##
+##  Date      : Jun 01, 2023                                                  ##
 ##  License   : GPLv3                                                         ##
 ##  Author    : mateus.digital <hello@mateus.digital>                         ##
 ##  Copyright : mateus.digital - 2023                                         ##
 ##                                                                            ##
 ##  Description :                                                             ##
-##    To publish in github pages we need to:                                  ##
-##      1 - Check to the publish branch.                                      ##
-##      2 - Copy the needed resources to docs/                                ##
-##      3 - Get info of current version and build number.                     ##
-##      4 - Insert build and version info into the index.html                 ##
-##      5 - Make a commit (push is manual)                                    ##
+##    Builds the website for the project.                                     ##
 ##---------------------------------------------------------------------------~##
 
+set -e; ## break on errors.
 
 readonly SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)";
 readonly ROOT_DIR="$(dirname "$SCRIPT_DIR")";
-
 
 ## retrive build and version.
 readonly VERSION="$(git describe --abbrev=0 --tags)";
@@ -37,33 +32,25 @@ readonly CURR_BUILD="$(cat "${ROOT_DIR}/.buildno")";
 readonly NEXT_BUILD="$(( CURR_BUILD + 1 ))";
 readonly DATE="$(date +'%H:%M:%S %d-%m-%Y - %Z')";
 
-
-git checkout publish;
-
 ## clean dir.
-rm -rf   "${ROOT_DIR}/docs/";
-mkdir -p "${ROOT_DIR}/docs/";
+rm -rf   "${ROOT_DIR}/out/";
+mkdir -p "${ROOT_DIR}/out/";
 
 ## copy things.
 cp -R "${ROOT_DIR}/html/"* \
-    "${ROOT_DIR}/docs/"
+    "${ROOT_DIR}/out/"
 
-## remove git repos from docs/
-find "$ROOT_DIR/docs" -iname "*.git*" -type d | xargs rm -rf;
-
+## remove git repos from out/
+find "$ROOT_DIR/out" -iname "*.git*" -type d | xargs rm -rf;
 
 ## update the index with version and build number.
-cat "${ROOT_DIR}/docs/index.html"         \
+cat "${ROOT_DIR}/out/index.html"          \
     | sed s/"_version_"/"${VERSION}"/g    \
     | sed s/"_build_"/"${NEXT_BUILD}"/g   \
-    > "${ROOT_DIR}/docs/index.html.tmp"   \
+    > "${ROOT_DIR}/out/index.html.tmp"    \
     && mv                                 \
-        "${ROOT_DIR}/docs/index.html.tmp" \
-        "${ROOT_DIR}/docs/index.html"     \
+        "${ROOT_DIR}/out/index.html.tmp"  \
+        "${ROOT_DIR}/out/index.html"      \
     ;
 
-echo "${NEXT_BUILD}" > "${ROOT_DIR}/.buildno";
-
-## Commit
-git add docs/;
-git commit -m "$0 - build: $NEXT_BUILD - $DATE";
+echo "${NEXT_BUILD}" > "${ROOT_DIR}/.buildno"; ## update build no.
---
+++ scripts/deploy.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+##~---------------------------------------------------------------------------##
+##                               *       +                                    ##
+##                         '                  |                               ##
+##                     ()    .-.,="``"=.    - o -                             ##
+##                           '=/_       \     |                               ##
+##                        *   |  '=._    |                                    ##
+##                             \     `=./`,        '                          ##
+##                          .   '=.__.=' `='      *                           ##
+##                 +                         +                                ##
+##                      O      *        '       .                             ##
+##                                                                            ##
+##  File      : deploy.sh                                                     ##
+##  Project   : flappy_gb                                                     ##
+##  Date      : Jun 01, 2023                                                  ##
+##  License   : GPLv3                                                         ##
+##  Author    : mateus.digital <hello@mateus.digital>                         ##
+##  Copyright : mateus.digital - 2023                                         ##
+##                                                                            ##
+##                                                                            ##
+##  Description :                                                             ##
+##   Deploys the output of scripts/build-static.sh to the remote server.      ##
+##   Current user should have remote ssh keys installed on the server.        ##
+##---------------------------------------------------------------------------~##
+
+set -e; ## break on errors
+
+
+##
+##  Directories
+##
+
+##------------------------------------------------------------------------------
+readonly SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)";
+readonly ROOT_DIR="$(dirname "$SCRIPT_DIR")";
+
+readonly SOURCE_FOLDER="${ROOT_DIR}/out";
+readonly REMOTE_SERVER="mateus@mateus.digital";
+readonly REMOTE_FOLDER="/var/www/el_jamon_volador";
+
+readonly curr_build=$(                  \
+    cat "${ROOT_DIR}/pages/index.js" |  \
+    grep "const build = "            |  \
+    cut -d" " -f4 | tr -d ";"           \
+);
+
+echo "==> CURR BUILD: $curr_build";
+
+rsync -avz                                       \
+      --delete "${SOURCE_FOLDER}/"               \
+      -e ssh "${REMOTE_SERVER}:${REMOTE_FOLDER}" \
+    ;
